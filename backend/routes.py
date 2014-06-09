dc4148418702af93f7ca8191de7b12c6ebc1ac55
Mayank Jain <mjmayank@gmail.com>
2014-06-09 15:10:13 -0700

    simple template

--- backend/routes.py
+++ backend/routes.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import os
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -6,6 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/index', methods=['GET'])
+def index():
+	return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
