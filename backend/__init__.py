b94fee3e1dd34ef6bde0b4a50d84e0d9e5a22c0c
Peter Andringa <peter9221@gmail.com>
2014-06-12 22:53:26 -0400

    initial setup?

--- backend/__init__.py
+++ backend/__init__.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 client = pymongo.MongoClient("localhost", 27017)
 app.db = client.backend
 
+db = app.db
+students = db.students
+mentors = db.mentors
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
