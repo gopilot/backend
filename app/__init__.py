f1a95acd70406da9369790349fd672258b453509
Gabe Boning <gboning@gmail.com>
2014-06-16 22:51:17 -0400

    use mongo database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask, request
+import os
 import pymongo
 
 app = Flask(__name__)
 
-client = pymongo.MongoClient("localhost", 27017)
-db = client.backend
+mongo_uri = os.getenv("MONGOHQ_URL", "mongodb://localhost:27017/backend")
+
+mongo_db = mongo_uri.split("/")[-1]
+
+client = pymongo.MongoClient(mongo_uri)
+db = client[mongo_db]
 
 # Config variables
 Flask.secret_key = 'a091jv730bnsqf92pt893bndsk' # super-random string
