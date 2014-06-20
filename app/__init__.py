59c5d9edc82c88d04afa849bd24f64a913ff8019
Peter Andringa <peter9221@gmail.com>
2014-06-20 00:14:05 -0400

    remove tests for now

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,13 @@ from urlparse import urlparse
 
 app = Flask(__name__)
 
+sessions = None
+db = None
+
 mongo_url = urlparse( os.getenv("MONGOHQ_URL", "mongodb://localhost:27017/backend") )
 
 mongo_db = mongo_url.path.replace('/', '')
 mongo_client = pymongo.MongoClient( mongo_url.geturl() )
-db = mongo_client[mongo_db]
-
 
 redis_url = urlparse( os.getenv("REDISCLOUD_URL", "redis://localhost:6379") )
 sessions = redis.Redis(host=redis_url.hostname, port=redis_url.port, password=redis_url.password)
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from app import app
+
 app.run(debug=True)
 
 
