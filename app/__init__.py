e1780d6d19f6cc476adb0cd5dbf8712f3ce8edb4
Peter Andringa <peter9221@gmail.com>
2014-06-21 00:23:59 -0400

    remove logs now that heroku is fixed

--- app/__init__.py
+++ app/__init__.py
@@ -12,17 +12,12 @@ app.config['REDIS_URL'] = os.getenv('REDIS_URL',	'redis://localhost:6379')
 app.config['REDIS_DB']	= int(os.getenv('REDIS_DB',		'0'))
 app.config['DEBUG']			= bool(os.getenv('DEBUG', True))
 app.config['TESTING']		= bool(os.getenv('TESTING', False))
-print app.config
+#print app.config
 
-print 'start mongo'
 mongo_client = pymongo.MongoClient( app.config['MONGO_URL'] )
-print 'mongo client', mongo_client
 db = mongo_client[ app.config['MONGO_DB'] ]
-print 'mongo db', db
 redis_url = urlparse( app.config['REDIS_URL'] )
-print 'redis url', redis_url
 sessions = redis.Redis(host=redis_url.hostname, port=redis_url.port, password=redis_url.password, db=app.config['REDIS_DB'])
-print 'sessions', sessions
 
 ## Used by tests to change databases after updating config values
 def update_dbs():
@@ -40,9 +35,6 @@ app.register_blueprint(UserBlueprint, url_prefix="/users")
 from events import events as EventBlueprint
 app.register_blueprint(EventBlueprint, url_prefix="/events")
 
-print "registered blueprints"
-
 @app.route('/')
 def index():
-	print 'in index'
 	return render_template("index.html")
