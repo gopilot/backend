a93ea4a0ef2f6a4ac0acac3cfc9296a985dd95ba
Peter Andringa <peter9221@gmail.com>
2014-06-21 00:21:59 -0400

    change MONGO_URL to MONGOHQ_URL

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from urlparse import urlparse
 
 app = Flask(__name__)
 
-app.config['MONGO_URL'] = os.getenv('MONGO_URL',	'mongodb://localhost:27017')
+app.config['MONGO_URL'] = os.getenv('MONGOHQ_URL',	'mongodb://localhost:27017')
 app.config['MONGO_DB']	= os.getenv('MONGO_DB',		'backend')
 app.config['REDIS_URL'] = os.getenv('REDIS_URL',	'redis://localhost:6379')
 app.config['REDIS_DB']	= int(os.getenv('REDIS_DB',		'0'))
