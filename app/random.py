911e6eea54c02a6dcf81696bef3a9f1562f4a239
Peter Andringa <peter9221@gmail.com>
2014-06-21 00:05:45 -0400

    remove unessesary

--- app/random.py
+++
@@ -1,4 +0,0 @@
-from pymongo import mongo_client
-## Value copied from heroku's website:
-mongo_client = mongo_client.MongoClient( 'mongodb://heroku:au50UY2y2vx-pVXyvDey5w6Cj8cSCvTDTcGUK4PMQtCC4-aJkpLvmRElasCb8KW3Uwg7i_vn3r9U0Ibjz5h2YA@kahana.mongohq.com:10079/app26071927' )
-print mongo_client
