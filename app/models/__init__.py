61717c6eb2486dce5170b885ae640156c7dad39a
Peter Andringa <peter9221@gmail.com>
2014-10-04 16:10:58 -0400

    import in models?

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
-import os
-import glob
-__all__ = [os.path.basename(
-    f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*.py")]
+import users
+import events
+import posts
+import projects
