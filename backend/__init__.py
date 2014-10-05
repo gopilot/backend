e992c40b2098d885a90ed8f23516e8c5df79a506
Peter Andringa <peter9221@gmail.com>
2014-10-04 20:22:15 -0400

    test redis on boot

--- backend/__init__.py
+++ backend/__init__.py
@@ -42,6 +42,10 @@ def start():
 	global sessions
 	sessions = redis.from_url(app.config['REDIS_URL'])
 	print("Connected to Redis")
+	sessions.set('testing-redis', 'test')
+	test = sessions.get('testing-redis')
+	if test != 'test':
+		print("ERROR: REDIS NOT WORKING!!")
 
 	global AuthBlueprint
 	AuthBlueprint = Blueprint('auth', __name__)
