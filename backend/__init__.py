13dd3daa851c49351b0ce208ada177f24fe4bc8b
Peter Andringa <peter9221@gmail.com>
2014-10-18 17:23:55 -0400

    propogate exceptions

--- backend/__init__.py
+++ backend/__init__.py
@@ -22,6 +22,8 @@ app.config['TESTING']		= bool(os.getenv('TESTING', False))
 app.config['PRODUCTION']	= bool(os.getenv('PRODUCTION', False))
 app.config['STRIPE_KEY']	= os.getenv('STRIPE_KEY', '')
 app.config['SENDGRID_PASS'] = os.getenv('SENDGRID_PASS', '')
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
 
 if app.config['PRODUCTION']:
 	app.config['TESTING'] = False
