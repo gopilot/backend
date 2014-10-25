d4e588305450312d557fe553c79a6b890c1b9dd5
Peter Andringa <peter9221@gmail.com>
2014-10-24 23:07:40 -0400

    more logs

--- backend/__init__.py
+++ backend/__init__.py
@@ -42,12 +42,9 @@ app.debug = False
 
 ADMINS = ['peter@gopilot.org']
 
-@app.before_first_request
-def setup_logging():
-    if not app.debug:
-        # In production mode, add log handler to sys.stderr.
-        app.logger.addHandler(logging.StreamHandler())
-        app.logger.setLevel(logging.INFO)
+import logging, sys
+logging.basicConfig(stream=sys.stderr)
+app.logger.addHandler(logging.StreamHandler(stream=sys.stderr))
 
 def start():
 	print("Booting up...")
