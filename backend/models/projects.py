8c2f691b73218e94d5d7eb40ae069804c2b4b26f
Peter Andringa <peter9221@gmail.com>
2015-03-04 23:10:28 -0500

    make project name not required

--- backend/models/projects.py
+++ backend/models/projects.py
@@ -3,6 +3,6 @@ import mongoengine as orm
 from . import document, users, events
 
 class Project(document.Document):
-    name = orm.StringField(required=True)
+    name = orm.StringField()
     event = orm.ReferenceField(events.Event)
     team = orm.ListField( orm.ReferenceField(users.Student), default=[] )
