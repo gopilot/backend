773923736ed1aeadc3194bd02581ce2c4b0a10e1
Peter Andringa <peter9221@gmail.com>
2015-03-05 15:03:26 -0500

    fix

--- backend/models/posts.py
+++ backend/models/posts.py
@@ -3,12 +3,12 @@ import mongoengine as orm
 from . import document, users, events
 
 class Post(document.Document):
-    event = orm.ReferenceField(events.Event)
-    author = orm.ReferenceField(users.Organizer)
+    event = orm.ReferenceField(events.Event, required=True)
+    author = orm.ReferenceField(users.Organizer, required=True)
     time = orm.DateTimeField(required=True)
 
     title = orm.StringField(required=True)
-    notif = orm.StringField(required=True) ## For push notifications
+    notif = orm.StringField() ## For SMS notifications
     
     image = orm.URLField()
     body = orm.StringField()
