e97115516d99ceedab616232764b13cf39a82579
Peter Andringa <peter9221@gmail.com>
2015-07-15 22:15:30 -0400

    only send one email

--- backend/controllers/registration.py
+++ backend/controllers/registration.py
@@ -243,37 +243,20 @@ def register(event_id):
     message = sendgrid.Mail();
     message.add_to(user.name+"<"+user.email+">")
     message.set_from("Pilot <fly@gopilot.org>")
-    
-    if user.complete:
-        message.set_subject("Your "+event.name+" registration.")
-        email_html = render_template('registration.html',
-            event_name=event.name,
-            first_name=user.name.split(' ')[0], 
-            subject="Your "+event.name+" registration."
-        )
-        message.set_html(email_html)
-        email_text = render_template('registration.txt',
-            event_name=event.name,
-            first_name=user.name.split(' ')[0], 
-            subject="Your "+event.name+" registration."
-        )
-        message.set_text(email_text)
-    else:
-        message.set_subject("Complete your "+event.name+" registration")
-        email_html = render_template('complete_registration.html',
-            event_name=event.name,
-            first_name=user.name.split(' ')[0], 
-            subject="Complete your "+event.name+" registration.",
-            registration_link="http://epa.gopilot.org/complete.html?token="+user.completion_token
-        )
-        message.set_html(email_html)
-        email_text = render_template('complete_registration.txt',
-            event_name=event.name,
-            first_name=user.name.split(' ')[0], 
-            subject="Complete your "+event.name+" registration.",
-            registration_link="http://epa.gopilot.org/complete.html?token="+user.completion_token
-        )
-        message.set_text(email_text)
+
+    message.set_subject("Your "+event.name+" registration.")
+    email_html = render_template('registration.html',
+        event_name=event.name,
+        first_name=user.name.split(' ')[0], 
+        subject="Your "+event.name+" registration."
+    )
+    message.set_html(email_html)
+    email_text = render_template('registration.txt',
+        event_name=event.name,
+        first_name=user.name.split(' ')[0], 
+        subject="Your "+event.name+" registration."
+    )
+    message.set_text(email_text)
 
     if not app.config['TESTING']:
         status, msg = sg.send(message)
